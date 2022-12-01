a6d0f73111175bfab9f4ccfc0decdd85845370b7
Juan Mosquella <jmosquella11@gmail.com>
2022-12-01 00:20:10 -0300

    actulizados controller hotels

--- controllers/hotel-controller.js
+++ controllers/hotel-controller.js
@@ -8,10 +8,40 @@ const insertHotels = async (req, res) => {
   });
 };
 
-const getAllHotels = (req, res) => {
-  res.json({
-    msg: "hola mundo",
-  });
+const getHotels = async (req, res) => {
+  try {
+    const hotels = await Hotel.find({});
+
+    res.status(200).json({
+      hotels,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error,
+    });
+  }
 };
 
-export { insertHotels, getAllHotels };
+const getHotel = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    const findHotel = await Hotel.findById(id);
+
+    if (!findHotel)
+      return res.status(404).json({
+        msg: "El hotel solicitado no existe",
+      });
+
+    res.status(200).json({
+      findHotel,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error,
+    });
+  }
+};
+
+export { insertHotels, getHotels, getHotel };
