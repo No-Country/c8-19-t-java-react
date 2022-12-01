40bd38647b131fb299e49753fe9b27b5282da461
Juan Mosquella <jmosquella11@gmail.com>
2022-12-01 13:30:57 -0300

    agregado endpoint de hotel

--- routes/hotel-route.js
+++ routes/hotel-route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getHotels, insertHotels } from "../controllers/hotel-controller.js";
+import {
+  getHotel,
+  getHotels,
+  insertHotels,
+} from "../controllers/hotel-controller.js";
 const router = Router();
 
 router.use("/insertHotels", insertHotels);
 
 router.get("/", getHotels);
+
+router.get("/:id", getHotel);
 
 export default router;
