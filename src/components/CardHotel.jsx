526e92bacafb85bdd5294b038780b5239aa9f125
Juan Mosquella <jmosquella11@gmail.com>
2022-12-07 02:22:50 -0300

    mergeados cambios

--- src/components/CardHotel.jsx
+++ src/components/CardHotel.jsx
@@ -1,6 +1,5 @@
-import { Rating } from '@mui/material';
+import { Rating } from "@mui/material";
 import { AiOutlineDelete, AiOutlineHeart, AiOutlineWifi } from "react-icons/ai";
-<<<<<<< HEAD
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useRemoveFavMutation } from "../redux/api/sunnyApi";
@@ -17,19 +16,9 @@ const CardHotel = ({ hotel, addedToFavs }) => {
   const { days } = useContext(DaysContext);
 
   console.log(days);
-=======
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { useRemoveFavMutation } from '../redux/api/sunnyApi';
-import { useAddToFavMutation } from '../redux/api/sunnyApi';
 
-const CardHotel = ({ hotel, addedToFavs }) => {
-    const [addToFav] = useAddToFavMutation();
->>>>>>> 4d31d66e622e2869088e08f68d19f4fe1c704e31
+  const [removeFav] = useRemoveFavMutation();
 
-    const [removeFav] = useRemoveFavMutation();
-
-<<<<<<< HEAD
   const { user, token } = useSelector((state) => state.auth);
 
   const handleFav = async (e) => {
@@ -40,25 +29,15 @@ const CardHotel = ({ hotel, addedToFavs }) => {
       console.log("debes estar autenticado");
       return;
     }
-=======
-    const user = useSelector(state => state.auth.user);
 
-    const handleFav = async e => {
-        e.preventDefault();
->>>>>>> 4d31d66e622e2869088e08f68d19f4fe1c704e31
-
-        const newFavHotel = {
-            id: hotel._id,
-            name: user,
-        };
-
-        addToFav(newFavHotel);
+    const newFavHotel = {
+      id: hotel._id,
+      name: user,
     };
 
-    const handleFavDelete = e => {
-        e.preventDefault();
+    addToFav(newFavHotel);
+  };
 
-<<<<<<< HEAD
   const handleFavDelete = (e) => {
     e.preventDefault();
     setbuttonClicked(true);
@@ -67,79 +46,49 @@ const CardHotel = ({ hotel, addedToFavs }) => {
       console.log("debes estar autenticado");
       return;
     }
-=======
-        const newFavHotel = {
-            id: hotel._id,
-            name: user,
-        };
->>>>>>> 4d31d66e622e2869088e08f68d19f4fe1c704e31
 
-        removeFav(newFavHotel);
+    const newFavHotel = {
+      id: hotel._id,
+      name: user,
     };
 
-    return (
-        <Link to={`/${hotel._id}`}>
-            <div className='flex flex-col md:flex-row gap-2 items-center bg-white rounded-xl overflow-hidden'>
-                <figure className='w-[400px] h-[240px] object-cover bg-slate'>
-                    <img
-                        className='w-full h-full  object-cover bg-slate'
-                        src={hotel.pictures[0]}
-                        alt={hotel.title}
-                    />
-                </figure>
-                <div className='flex justify-between items-center w-full p-4'>
-                    <div className='flex flex-col '>
-                        <h4 className='text-slate font-bold text-xl'>
-                            {hotel.title}
-                        </h4>
-                        <Rating
-                            name='read-only'
-                            value={hotel.rating}
-                            precision={0.5}
-                            readOnly
-                        />
-                        <span className='bg-blue text-white text-sm font-semibold rounded-md w-fit px-4 py-2 my-2'>
-                            {hotel.rating}
-                        </span>
-                        <div>
-                            <span className='text-slate font-semibold text-md'>
-                                Services:
-                            </span>
-                            <ul className='flex gap-3 text-slate capitalize flex-wrap pt-2'>
-                                {Object.keys(hotel.services).map(
-                                    (service, index) => (
-                                        <li className='text-sm' key={index}>
-                                            {service}
-                                        </li>
-                                    )
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-16 justify-between items-end h-full'>
-                        <button className='bg-white shadow-md  p-1 w-[50px] h-[50px] flex justify-center items-center rounded-full'>
-                            {addedToFavs ? (
-                                <AiOutlineDelete
-                                    onClick={handleFavDelete}
-                                    className='text-slate/40 text-2xl font-thin'
-                                />
-                            ) : (
-                                <AiOutlineHeart
-                                    onClick={handleFav}
-                                    className=' text-slate/40 text-2xl font-thin'
-                                />
-                            )}
-                        </button>
-                        <button className='hidden md:flex bg-blue self-end hover:bg-blue/75 duration-300  text-white capitalize font-semibold w-full rounded-xl p-4 whitespace-nowrap'>
-                            ver disponibilidad
-                        </button>
-                    </div>
-                </div>
-                <button className='block md:hidden bg-blue self-end hover:bg-blue/75 duration-300 text-white capitalize font-semibold w-full rounded-xl p-4 whitespace-nowrap'>
-                    ver disponibilidad
-                </button>
+    removeFav(newFavHotel);
+  };
+
+  return (
+    <Link to={`/${hotel._id}`}>
+      <div className="flex gap-2  bg-white rounded-xl overflow-hidden">
+        <figure className="w-[400px] h-[240px] object-cover bg-slate">
+          <img
+            className="w-full h-full  object-cover bg-slate"
+            src={hotel.pictures[0]}
+            alt={hotel.title}
+          />
+        </figure>
+        <div className="flex justify-between items-center w-full p-4">
+          <div className="flex flex-col ">
+            <h4 className="text-slate font-bold text-xl">{hotel.title}</h4>
+            <Rating
+              name="read-only"
+              value={hotel.rating}
+              precision={0.5}
+              readOnly
+            />
+            <span className="bg-blue text-white text-sm font-semibold rounded-md w-fit px-4 py-2 my-2">
+              {hotel.rating}
+            </span>
+            <div>
+              <span className="text-slate font-semibold text-md">
+                Services:
+              </span>
+              <ul className="flex gap-3 text-slate capitalize ">
+                {Object.keys(hotel.services).map((service, index) => (
+                  <li className="text-sm" key={index}>
+                    {service}
+                  </li>
+                ))}
+              </ul>
             </div>
-<<<<<<< HEAD
           </div>
           <div className="flex flex-col justify-between items-end h-full relative">
             <button className="bg-white shadow-md  p-1 w-[50px] h-[50px] flex justify-center items-center rounded-full">
@@ -173,9 +122,5 @@ const CardHotel = ({ hotel, addedToFavs }) => {
       </div>
     </Link>
   );
-=======
-        </Link>
-    );
->>>>>>> 4d31d66e622e2869088e08f68d19f4fe1c704e31
 };
 export default CardHotel;
--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -4,18 +4,22 @@ import Results from "../components/Results";
 
 const ResultsPage = () => {
   const [active, setActive] = useState(false);
-  
-  const handleToggleFilters =()=>{
-    setActive(!active)
-  }
+
+  const handleToggleFilters = () => {
+    setActive((prev) => !prev);
+  };
   return (
     <section className="min-h-screen bg-gray pt-[100px] mb-6 ">
       <div className="grid grid-cols-4 min-h-screen md:container gap-4 ">
         <div className="hidden md:block md:col-span-1 bg-white h-fit">
           <Filters />
         </div>
-        <div className={`${active ? 'block' : 'hidden'} absolute md:hidden w-10/12 bg-white h-fit z-10 transition ease-in-out delay-300`}>
-          <Filters onToggleFilters={handleToggleFilters}/>
+        <div
+          className={`${
+            active ? "block" : "hidden"
+          } absolute md:hidden w-10/12 bg-white h-fit z-10 transition ease-in-out delay-300`}
+        >
+          <Filters onToggleFilters={handleToggleFilters} />
         </div>
         <div className="col-span-4 md:col-span-3 ">
           <Results onToggleFilters={handleToggleFilters} viewFilters={active} />
